74f2b9cc3e0cdd24e160ef1099f04f2391bcc7d5
William <william.k.nygard@icloud.com>
2017-10-04 10:57:28 +0000

    Reverse order of tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.reverse_order
   end
 
   # GET /tweets/1
